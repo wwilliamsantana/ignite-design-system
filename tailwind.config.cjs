7ccce06587cbfc3590c551bf40577052d2774269
William Silva <euwilliamsantana@gmail.com>
2022-10-14 14:22:09 -0300

    Components - TextInput

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,11 @@ module.exports = {
       "2xl": 32,
     },
     colors:{
+      transparent: "transparent",
+
       black: "#000",
       white: "#FFF",
+
       "gray-900": "#121214",
       "gray-800": "#202024",
       "gray-400": "#7c7c8a",
